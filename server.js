d11f31d1fd06da455753ee3b129d6299fa95f0dc
iamanditorres <aotorres2@up.edu.ph>
2024-01-10 13:36:49 +0800

    Add cors

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ const tweetRoutes = require('./routes/tweets')
 // create an express app
 const app = express()
 
+const cors=require("cors");
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 // middleware
 // looks at any requests if it has a body (data) and 
 // passes and attaches it to the request obj
