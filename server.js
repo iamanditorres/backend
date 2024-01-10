8c4fbd1f4f2a07632dc86663178b073c407dbb78
iamanditorres <aotorres2@up.edu.ph>
2024-01-10 14:07:33 +0800

    Removed methods

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const corsOptions ={
    origin:'*', 
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
+   method:['GET', 'PUT', 'POST', 'PATCH']
 }
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
